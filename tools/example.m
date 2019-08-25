70eb6c0fa68bc0830bcf51d7f52aa7dce8e0ae52
Ruud (Rudy) van den Brink <30622165+rudyvdbrink@users.noreply.github.com>
2019-08-25 13:36:06 +0200

    Update example.m

--- tools/example.m
+++ tools/example.m
@@ -1,3 +1,3 @@
-load CHRNB3.mat %load the data (this also contains the surfaces)
+load CHRNB3.mat %load the data (this also contains the surfaces; make sure that the file folder 'surface' is on matlab's path)
 cortsurfr(gr,rdat) %plot the right hemisphere
 cortsurfl(gl,ldat) %plot the left hemisphere
