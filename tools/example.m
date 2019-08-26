11aa9e6d4516a252ef3b9c3dcd1a5f2368613565
Ruud (Rudy) van den Brink <30622165+rudyvdbrink@users.noreply.github.com>
2019-08-26 11:11:06 +0200

    Update example.m

--- tools/example.m
+++ tools/example.m
@@ -1,3 +1,8 @@
-load CHRNB3.mat %load the data (this also contains the surfaces; make sure that the file folder 'surface' is on matlab's path)
+
+%for this example to run, please make sure that matlab has access to:
+%  - the gifti toolbox (in the same folder as this script)
+%  - the data itself, in the folder 'surface')
+
+load CHRNB3.mat %load the data 
 cortsurfr(gr,rdat) %plot the right hemisphere
 cortsurfl(gl,ldat) %plot the left hemisphere
