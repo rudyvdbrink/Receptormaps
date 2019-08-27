9c35c02a6488caddde9c6b54375a8c4ee54bde50
Ruud (Rudy) van den Brink <30622165+rudyvdbrink@users.noreply.github.com>
2019-08-27 10:37:22 +0200

    Update example.m

--- tools/example.m
+++ tools/example.m
@@ -3,6 +3,6 @@
 %  - the gifti toolbox (in the same folder as this script)
 %  - the data itself, in the folder 'surface')
 
-load CHRNB3.mat %load the data 
+load CHRNB3.mat %load the data (here, nicotinic acetylcholine receptor B3)
 cortsurfr(gr,rdat) %plot the right hemisphere
 cortsurfl(gl,ldat) %plot the left hemisphere
